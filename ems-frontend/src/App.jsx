c914778b3ab5e8c13cb15ff7b696f095b889d25c
haroldo-barros <debarrosharoldo@gmail.com>
2026-09-04 16:47:37 -0400

    configure rooting

--- ems-frontend/src/App.jsx
+++ ems-frontend/src/App.jsx
@@ -3,14 +3,20 @@ import HelloWorld from './HelloWorld'
 import ListEmployeeComponent from './components/ListEmployeeComponent'
 import HeaderComponent from './components/HeaderComponent'
 import FooterComponent from './components/FooterComponent'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
 
 function App() {
 
   return (
       <>
-        <HeaderComponent />
-        <ListEmployeeComponent />
-        <FooterComponent />
+          <BrowserRouter>
+            <HeaderComponent />
+                <Routes>
+                    <Route path='/' element = { <ListEmployeeComponent /> }></Route>
+                    <Route path='/employees' element = { <ListEmployeeComponent /> }></Route>
+                </Routes>
+            <FooterComponent />
+          </BrowserRouter>
       </>
   )
 }
