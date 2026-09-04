f5b101d43ae49c6b69cc0538d7c1865b18e5282c
haroldo-barros <debarrosharoldo@gmail.com>
2026-09-04 16:38:13 -0400

    adding header and footer

--- ems-frontend/src/App.jsx
+++ ems-frontend/src/App.jsx
@@ -1,11 +1,17 @@
 import './App.css'
 import HelloWorld from './HelloWorld'
 import ListEmployeeComponent from './components/ListEmployeeComponent'
+import HeaderComponent from './components/HeaderComponent'
+import FooterComponent from './components/FooterComponent'
 
 function App() {
 
   return (
-    <ListEmployeeComponent />
+      <>
+        <HeaderComponent />
+        <ListEmployeeComponent />
+        <FooterComponent />
+      </>
   )
 }
 
