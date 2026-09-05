9bc7079060e59284014b1723d22754c6891cb776
haroldo-barros <debarrosharoldo@gmail.com>
2026-09-04 20:25:09 -0400

    update employee feature - connect to Get Employee REST API

--- ems-frontend/src/components/EmployeeComponent.jsx
+++ ems-frontend/src/components/EmployeeComponent.jsx
@@ -1,5 +1,5 @@
 import React, {useState, useEffect} from 'react'
-import {createEmployee, getEmployee} from '../services/EmployeeService'
+import {createEmployee, getEmployee, updateEmployee} from '../services/EmployeeService'
 import {useNavigate, useParams} from 'react-router-dom'
 
 const EmployeeComponent = () => {
@@ -30,15 +30,27 @@ const EmployeeComponent = () => {
         }
     }, [id])
 
-    function saveEmployee(e){
+    function saveOrUpdateEmployee(e){
         e.preventDefault();
 
         if(validateForm()){
             const employee = {firstName, lastName, email}
-            createEmployee(employee).then((response) =>{
-                console.log(response.data);
-                navigator('/employees')
-            })
+            if(id){
+                updateEmployee(id, employee).then((response) => {
+                    console.log(response.data);
+                    navigator('/employees')
+                }).catch(error => {
+                    console.error(error);
+                })
+            }else{
+                createEmployee(employee).then((response) =>{
+                    console.log(response.data);
+                    navigator('/employees')
+                }).catch(error => {
+                    console.error(error);
+                })
+            }
+
         }
 
     }
@@ -128,7 +140,7 @@ const EmployeeComponent = () => {
                                 </input>
                                 {errors.email && <div className='invalid-feedback'>{errors.email}</div>}
                             </div>
-                            <button className="btn btn-success" onClick={saveEmployee}>Submit</button>
+                            <button className="btn btn-success" onClick={saveOrUpdateEmployee}>Submit</button>
                         </form>
                     </div>
                 </div>
